178f5aa64926ae2801e6050f71722199b2f0568f
Diana Utan <didiutzi@gmail.com>
2020-11-28 17:12:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,19 +3,16 @@ def prime? (num)
  if num < 2
     return false 
   end
-<<<<<<< HEAD
  for i in (2..(num - 1))
   if num % i == 0
    return false
   end 
  end
  return true
-=======
   for i in (2..(num - 1))
     if num % i == 0
       return false
     end 
   end
   return true
->>>>>>> e3fa37051f2998563031724c0c4f03c3597be5af
 end
